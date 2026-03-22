531f8406413384cf65dc5d0964f220906fd5f5d3
Jordan Pitlor <jordan@pitlor.dev>
2026-03-22 15:09:59 -0400

    typo

--- files/sunshine/99-install-heroic-launcher.sh
+++ files/sunshine/99-install-heroic-launcher.sh
@@ -2,4 +2,4 @@
 
 echo "\e[36m  - Installing Heroic Launcher...\e[0m"
 curl -o /tmp/heroic.deb  https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.20.1/Heroic-2.20.1-linux-amd64.deb
-sudo dkpg -i /tmp/heroic.deb
+sudo dpkg -i /tmp/heroic.deb
