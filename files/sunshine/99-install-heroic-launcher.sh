b4549362ff9809a99f9e673a1f7c8d02eabc2f8b
Jordan Pitlor <jordan@pitlor.dev>
2026-03-22 15:28:39 -0400

    flatpak?

--- files/sunshine/99-install-heroic-launcher.sh
+++ files/sunshine/99-install-heroic-launcher.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-echo "\e[36m  - Installing Heroic Launcher...\e[0m"
-curl -o /tmp/heroic.deb  https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/releases/download/v2.20.1/Heroic-2.20.1-linux-amd64.deb
-sudo dpkg -i /tmp/heroic.deb
+echo -e "\e[35m*** Installing Heroic Games Launcher ***\e[0m"
+flatpak install flathub com.heroicgameslauncher.hgl
