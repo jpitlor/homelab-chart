1ee5feb0d1282319df0f05efa6ad108966449837
Jordan Pitlor <jordan@pitlor.dev>
2026-08-11 20:04:21 -0400

    Update version to 3.0.1

--- script/new-version.sh
+++ script/new-version.sh
@@ -6,7 +6,7 @@ git commit -am "Update version to $1"
 git push origin main
 
 # Push new version
-gcloud auth application-default print-access-token | helm registry login -u oauth2accesstoken --password-stdin https://us-central1-docker.pkg.dev
+gcloud auth application-default print-access-token | helm registry login -u oauth2accesstoken --password-stdin us-central1-docker.pkg.dev
 helm package .
 PACKAGE=$(ls *.tgz)
 helm push $PACKAGE oci://us-central1-docker.pkg.dev/dev-pitlor-homelab/homelab-chart
